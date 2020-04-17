b45409f7a0032cfbbf68dfec796bc1332fcb969c
amarilis <amarilis.alveo@modop.com>
2020-04-17 16:27:16 -0500

    Squashed commit of the following:
    commit 8554e1da5ac23b2b392695b8abe0ed305976e180
    Author: amarilis <amarilis.alveo@modop.com>
    Date:   Fri Apr 17 16:26:46 2020 -0500
    
        fix
    
    commit 75602e155027332a5b4af10aa68978c60b8fc54f
    Author: amarilis <amarilis.alveo@modop.com>
    Date:   Fri Apr 17 14:16:36 2020 -0500
    
        fixes
    
    commit 32b2939507ebaf75cf36651c85cdee34bfd950a1
    Author: amarilis <amarilis.alveo@modop.com>
    Date:   Fri Apr 17 13:27:14 2020 -0500
    
        files
    
    commit 5fb5dda511d69382ac6b8fa4d1ff580e1df2b0ed
    Author: amarilis <amarilis.alveo@modop.com>
    Date:   Tue Apr 14 20:08:35 2020 -0500
    
        project strucuture

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,29 +22,29 @@ class Home extends React.Component {
                   <li data-target="#carousel-example-1z" data-slide-to="2"></li>
               </ol>
 
-              <div class="carousel-inner" role="listbox">
+              <div className="carousel-inner" role="listbox">
     
-                  <div class="carousel-item active">
-                      <img class="d-block w-100" src={require('../images/frog.jpg')} alt="First slide"/>
+                  <div className="carousel-item active">
+                      <img className="d-block w-100" src={require('../images/frog.jpg')} alt="First slide"/>
                   </div>
                
-                  <div class="carousel-item">
-                      <img class="d-block w-100" src={require('../images/golden-frog.jpg')}  alt="Second slide"/>
+                  <div className="carousel-item">
+                      <img className="d-block w-100" src={require('../images/golden-frog.jpg')}  alt="Second slide"/>
                   </div>
                 
-                  <div class="carousel-item">
-                      <img class="d-block w-100" src={require('../images/black-frog.jpg')} alt="Third slide"/>
+                  <div className="carousel-item">
+                      <img className="d-block w-100" src={require('../images/black-frog.jpg')} alt="Third slide"/>
                   </div>
          
               </div>
      
               <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="sr-only">Previous</span>
               </a>
               <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="sr-only">Next</span>
               </a>
        
           </div>
